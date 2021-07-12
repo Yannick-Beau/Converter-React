22a35ac0c401577818d4dc68eab4d7e9d730f9ba
Yannick-Beau <beauyannick@gmail.com>
2021-07-12 20:16:02 +0200

    chall done

---
+++ src/components/Converter/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './converter.scss';
+
+
+const Converter = () => (
+  <header className="header">
+      <h1 className="header-title">Converter</h1>
+      <p className="header-info">1 euro</p>
+  </header>
+);
+
+export default Converter;
